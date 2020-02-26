a5230135775cd2bc65e8817515c44cf455c1afa9
Kalloyan Stoyanov <kalloyan@udel.edu>
2020-02-26 10:28:43 -0500

    Committing so I can pull the new code.

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,15 +2,46 @@ import ExampleObject from '../objects/exampleObject';
 
 export default class MainScene extends Phaser.Scene {
   private exampleObject: ExampleObject;
+  private background: Phaser.GameObjects.TileSprite;
+  private ship1: Phaser.GameObjects.Image;
+  private ship2: Phaser.GameObjects.Image;
+  private ship3: Phaser.GameObjects.Image;
 
   constructor() {
     super({ key: 'MainScene' });
   }
 
   create() {
+    this.background = this.add.tileSprite(0, 0, 256, 272, "background");
+    this.background.setOrigin(0, 0);
+
+    this.ship1 = this.add.image(20, 50, "ship1");
+    this.ship1.setScale(2);
+    this.ship1.flipY = true;
+    this.ship2 = this.add.image(50, 100, "ship1")
+    this.ship3 = this.add.image(100, 150, "ship1")
     this.exampleObject = new ExampleObject(this, 0, 0);
+    this.add.text(50, 50, "Start game", {font: "25px Arial", fill: "blue"});
   }
 
   update() {
+    this.moveShip(this.ship1, 1);
+    this.moveShip(this.ship2, 3);
+    this.moveShip(this.ship3, 2);
+
+    this.background.tilePositionY -= 0.5;
+  }
+
+  moveShip(ship, speed){
+    ship.y += speed;
+    if(ship.y > 272){
+      this.resetShipPos(ship);
+    }
+  }
+
+  resetShipPos(ship){
+    ship.y = 0;
+    let randomX: number = Phaser.Math.Between(0, 256);
+    ship.x = randomX;
   }
 }
--- src/scripts/scenes/preloadScene.ts
+++ src/scripts/scenes/preloadScene.ts
@@ -4,6 +4,10 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    this.load.image("background", "assets/background.png");
+    this.load.image("ship1", "assets/ship.png");
+    this.load.image("ship2", "assets/ship2.png");
+    this.load.image("ship3", "assets/ship3.png");
   }
 
   create() {
