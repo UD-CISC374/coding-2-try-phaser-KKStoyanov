3398f66eb79ea892b237a044ceaae5bf32b20057
Kalloyan Stoyanov <kalloyan@udel.edu>
2020-02-26 10:35:04 -0500

    Updates

---
+++ dev/precache-manifest.c61a555060b54402f735ecdbebd669eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-KKStoyanov/devassets/background.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-KKStoyanov/devassets/citations.md"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-KKStoyanov/devassets/ship.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-KKStoyanov/devassets/ship2.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-KKStoyanov/devassets/ship3.png"
+  },
+  {
+    "revision": "060a445cb78bf9fe8fec",
+    "url": "/coding-2-try-phaser-KKStoyanov/devgen.main.8a63400409585466a8db.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-KKStoyanov/devgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "289d80a1a05b0387f1f5720422958031",
+    "url": "/coding-2-try-phaser-KKStoyanov/devindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-KKStoyanov/devmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-KKStoyanov/devsw.js"
+  }
+]);
