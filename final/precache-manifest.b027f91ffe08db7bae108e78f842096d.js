a0b447998ddf7dfead47ccd9c576075bf6ce75f7
Kalloyan Stoyanov <kalloyan@udel.edu>
2020-02-28 09:15:43 -0500

    Updates

---
+++ final/precache-manifest.b027f91ffe08db7bae108e78f842096d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd0dcea47820063efe7dabd12e32b87",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/background.png"
+  },
+  {
+    "revision": "6be3462bcaa4a183b678800bb66d176c",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/beam.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/citations.md"
+  },
+  {
+    "revision": "ea801e18a4f14b0c4740b555536b414e",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/explosion.png"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/font/font.xml"
+  },
+  {
+    "revision": "ff70e55fbb2b70941d98b3e7ab1a01aa",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/player.png"
+  },
+  {
+    "revision": "05427110fd2c2de1d4738b31496a53c9",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/power-up.png"
+  },
+  {
+    "revision": "6387e4bedb194c28efe573c1cc5ce303",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/ship.png"
+  },
+  {
+    "revision": "4888982b102ce1a7c57fb9f8c129e63d",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/ship2.png"
+  },
+  {
+    "revision": "ea0ad2cda5a9dbbe332fd61e4c2f5700",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/ship2_1.png"
+  },
+  {
+    "revision": "15d30c88882683af9f5092c13dc13853",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/ship3.png"
+  },
+  {
+    "revision": "5896d72a8a287a6e5f3272bfb9eadca8",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/ship3_1.png"
+  },
+  {
+    "revision": "ccd0f68c9dc7cd68ea1e98bf43aa7c90",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalassets/ship_1.png"
+  },
+  {
+    "revision": "cc9e85858d9bacc4331d",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalgen.main.7bd6d868fe23028da14d.bundle.js"
+  },
+  {
+    "revision": "201b65bee9df6270f739",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalgen.vendors.a047ee0740c7d21714d9.bundle.js"
+  },
+  {
+    "revision": "57f3ff6c8e91abe01c82840bf9d4daef",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalindex.html"
+  },
+  {
+    "revision": "b4321e6f6a88a978d4964c0d1b432b03",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/coding-2-try-phaser-KKStoyanov/finalsw.js"
+  }
+]);
